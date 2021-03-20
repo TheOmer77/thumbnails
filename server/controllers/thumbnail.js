54d23c0615d92fb4fb86fe6872bdc1287d1a1c53
Omer <27678413+TheOmer77@users.noreply.github.com>
2021-03-20 16:35:33 +0200

    Finish the video thumbnail generator (I think)

--- server/controllers/thumbnail.js
+++ server/controllers/thumbnail.js
@@ -1,4 +1,6 @@
 const ffmpeg = require('fluent-ffmpeg');
+const ffprobe = require('ffprobe');
+const ffprobeStatic = require('ffprobe-static');
 
 exports.upload = (req, res) => {
   const { file } = req;
@@ -15,21 +17,46 @@ exports.upload = (req, res) => {
   });
 };
 
-/** NOTE: Sometimes this will take a while, so maybe it's best to just generate and save the thumnail
+/** NOTE: Sometimes this will take a while, so maybe it's best to just generate and save the thumbnail
  * on video upload, and then allow access to the pre generated thumbnail.
  * ALSO, THIS REQUIRES FFMPEG TO BE INSTALLED */
 exports.getThumbnail = async (req, res) => {
   const { filename } = req.params;
-  ffmpeg(
-    `uploads/${filename}`
-    // OR, HOW ABOUT THIS - YOU CAN USE A URL HERE!!!!
-    // 'https://res.cloudinary.com/dah7e44av/video/upload/v1616073895/youtube/pfft9yzwtfkdvqjp1spo.mp4'
-    // Or access another route in this server
-    // `${req.protocol}://${req.get('host')}${req.originalUrl}`
-  )
-    .setStartTime('0:15') // TODO: Find the length of a video, then take a screenshot from the middle
-    .outputOptions('-vframes 1')
-    .outputOptions('-f image2pipe')
-    .outputOptions('-vcodec png')
-    .writeToStream(res);
+  const filepath = `uploads/${filename}`;
+  /**
+   * For the file path, you can use a file from the uploads folder on this server.
+   * const filepath = `uploads/${filename}`;
+   * Or how about this - YOU CAN USE A URL!
+   * const filepath = 'https://res.cloudinary.com/dah7e44av/video/upload/v1616073895/youtube/pfft9yzwtfkdvqjp1spo.mp4'
+   * Or even access another route on this server...
+   * const filepath = `${req.protocol}://${req.get('host')}/some/other/route`
+   */
+
+  try {
+    // Get the duration of the video
+    const { streams } = await ffprobe(filepath, { path: ffprobeStatic.path });
+    const { duration } = streams[0];
+
+    ffmpeg(filepath)
+      // Take a screenshot from the middle of the video (duration / 2)
+      .setStartTime(parseFloat(duration) / 2)
+      .outputOptions('-vframes 1')
+      .outputOptions('-f image2pipe')
+      .outputOptions('-vcodec png')
+      .writeToStream(res);
+  } catch (error) {
+    res.status(500).send({ success: false, error });
+  }
+};
+
+exports.getInfo = async (req, res) => {
+  const { filename } = req.params;
+  try {
+    const info = await ffprobe(`uploads/${filename}`, {
+      path: ffprobeStatic.path,
+    });
+    res.send({ success: true, info });
+  } catch (error) {
+    res.status(500).send({ success: false, error });
+  }
 };
--- server/routes/thumbnail.js
+++ server/routes/thumbnail.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { upload, getThumbnail } = require('../controllers/thumbnail.js');
+const {
+  upload,
+  getThumbnail,
+  getInfo,
+} = require('../controllers/thumbnail.js');
 
 router.get('/thumbnail/:filename', getThumbnail);
+router.get('/info/:filename', getInfo);
 router.post('/upload', upload);
 
 module.exports = router;
